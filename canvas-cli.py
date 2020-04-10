92f3165d1387b620353188823f413c0a9efa3c93
GideonWolfe <wolfeg2@wwu.edu>
2020-04-09 18:51:33 -0700

    basic arg parsing

--- canvas-cli.py
+++ canvas-cli.py
@@ -2,6 +2,7 @@ import yaml
 import json
 import requests
 import io
+import sys
 
 conf = {}
 # Load config file
@@ -12,45 +13,65 @@ def load_config(config_file):
         except yaml.YAMLError as exc:
             print(exc)
 
+# Returns a JSON array with the users active courses
 def fetchActiveCourses(canvasDomain, canvasToken):
     url = canvasDomain+"api/v1/courses"
     HEADERS = {'Authorization': "Bearer "+canvasToken}
-    PARAMS = {'enrollment_state':'active'}
+    PARAMS = {'enrollment_state':'active', 'exclude_blueprint_courses':'true'}
     coursesRAW = requests.get(url, params=PARAMS, headers=HEADERS)
     courses = coursesRAW.json()
-    print(len(courses))
+    return(courses)
 
-    for course in courses:
-        print(course['name'])
-
-    #  for attr in courses[0].values():
-        #  print(attr)
-
-
+# Returns a JSON array of assignments for a given course
 def fetchAssignments(canvasDomain, canvasToken, courseId):
     url = canvasDomain+"api/v1/courses/"+str(courseId)+"/assignments"
     HEADERS = {'Authorization': "Bearer "+canvasToken}
     assignmentsRAW = requests.get(url, headers=HEADERS)
     assignments = assignmentsRAW.json()
-    for assignment in assignments:
-        print(assignment['name'])
     return(assignments)
 
-def main():
+def listAssignments(canvasDomain, canvasToken, courseId):
+    assignments = fetchAssignments(canvasDomain, canvasToken, courseId)
+    for assignment in assignments:
+        print(assignment['name'])
 
+def listActiveCourses(canvasDomain, canvasToken):
+    courses = fetchActiveCourses(canvasDomain, canvasToken)
+    for course in courses:
+        print(course['name'])
+
+def parseArgs():
+
+    canvasDomain = str(conf['default']['canvasdomain'])
+    canvasToken = str(conf['default']['canvastoken'])
+    courses = []
+    for course in conf['courses']:
+        courses.append(course)
+
+
+    if len(sys.argv) <= 1:
+        print("No arguments supplied")
+    elif sys.argv[1] == "list":
+        if len(sys.argv) < 3:
+            print("List what?")
+        elif sys.argv[2] == "courses":
+            print("Listing courses")
+            listActiveCourses(canvasDomain, canvasToken)
+        elif sys.argv[2] =="assignments":
+            if len(sys.argv) < 4:
+                print("Please supply a course ID")
+            else:
+                listAssignments(canvasDomain, canvasToken, courses[0])
+
+def main():
     # Load config file & save config file
     global conf
     with open("config.yaml", 'r') as stream:
         conf = yaml.safe_load(stream)
 
-    canvasDomain = str(conf['default']['canvasdomain'])
-    canvasToken = str(conf['default']['canvastoken'])
+    parseArgs()
 
-    courses = []
-    for course in conf['courses']:
-        courses.append(course)
 
-    #  fetchAssignments(canvasDomain, canvasToken, courses[0])
-    fetchActiveCourses(canvasDomain, canvasToken)
+
 
 main()
